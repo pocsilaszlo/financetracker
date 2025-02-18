79a278b454ff15c4ed8bcc59b9ef5f6edca90d07
pocsilaszlo <mrpro7773@gmail.com>
2025-02-18 14:04:29 +0100

    Commit

---
+++ backend/financetracker/src/main/java/hu/elte/financetracker/controller/TransactionController.java
@@ -0,0 +1,41 @@
+package hu.elte.financetracker.controller;
+
+import hu.elte.financetracker.entity.Category;
+import hu.elte.financetracker.entity.Transaction;
+import hu.elte.financetracker.service.FinanceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    private FinanceService financeService;
+
+    public TransactionController(FinanceService financeService) {}
+
+    @GetMapping("/all")
+    private ResponseEntity<?> getTransactions(){
+        return new ResponseEntity<>(financeService.getAllTransactions(), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    private ResponseEntity<?> addTransaction(@RequestBody Transaction transaction){
+        financeService.add(transaction);
+        return ResponseEntity.ok(Map.of("message", "Transaction added"));
+    }
+
+    @GetMapping("/categories/all")
+    private ResponseEntity<?> getCategories(){
+        return new ResponseEntity<>(financeService.getAllCategories(), HttpStatus.OK);
+    }
+
+    @PostMapping("/categories/add")
+    private ResponseEntity<?> addCategory(@RequestBody Category category){
+        financeService.add(category);
+        return ResponseEntity.ok(Map.of("message", "Category added"));
+    }
+}
--- backend/financetracker/src/main/java/hu/elte/financetracker/service/FinanceService.java
+++ backend/financetracker/src/main/java/hu/elte/financetracker/service/FinanceService.java
@@ -1,5 +1,34 @@
 package hu.elte.financetracker.service;
 
+import hu.elte.financetracker.entity.Category;
+import hu.elte.financetracker.entity.Transaction;
+import hu.elte.financetracker.repository.CategoryRepository;
+import hu.elte.financetracker.repository.TransactionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FinanceService {
 
+    private TransactionRepository transactionRepository;
+    private CategoryRepository categoryRepository;
+
+    public FinanceService(TransactionRepository transactionRepository, CategoryRepository categoryRepository) {}
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public void add(Transaction transaction) {
+        transactionRepository.save(transaction);
+    }
+
+    public void add(Category category) {
+        categoryRepository.save(category);
+    }
 }
